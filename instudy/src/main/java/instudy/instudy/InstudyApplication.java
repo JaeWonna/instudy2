cd191f6c48f206a0c69ad9ec95495ab56a47969e
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-03-14 01:39:43 +0900

    Initial commit

---
+++ instudy/src/main/java/instudy/instudy/InstudyApplication.java
@@ -0,0 +1,13 @@
+package instudy.instudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstudyApplication.class, args);
+	}
+
+}
---
+++ instudy/src/test/java/instudy/instudy/InstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package instudy.instudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
