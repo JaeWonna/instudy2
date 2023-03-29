5c07206b57b5cd382303205416fa1c15c0c5fceb
hido02 <bestdohee@gmail.com>
2023-03-30 00:06:16 +0900

    [front] FIX: Group.js 오류 해결중

--- frontend/src/pages/Group.js
+++ frontend/src/pages/Group.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {Container, Row, Col} from 'react-bootstrap';
-import HorizonLine from "../utils/HorizontalLine";
 
 const Group = () => {
     const [group, setGroup] = useState([]);
