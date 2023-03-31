4f772aaec0b7b722155e0148ea6d2ae12282e390
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-03-31 20:24:57 +0900

    [back]FEAT: Group 도메인 추가

---
+++ instudy/src/main/java/instudy/instudy/domain/Group.java
@@ -0,0 +1,37 @@
+package instudy.instudy.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Group {
+
+    @Id
+    private String id;
+    private String password;
+    private String groupName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
