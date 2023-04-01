cf2e9ccbb9cd0fe712518af2af0f797a83949482
hido02 <bestdohee@gmail.com>
2023-04-01 23:39:58 +0900

    [front] FIX: 그룹 모달 mdb 라이브러리 문제 해결

--- frontend/src/components/group/GroupMainView.js
+++ frontend/src/components/group/GroupMainView.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row } from 'react-bootstrap';
 import groupMember from '../../img/groupMember.jpg'; 
+import * as mdb from 'mdb-ui-kit';
+import { Modal } from 'mdb-ui-kit';
+import 'mdb-ui-kit/css/mdb.min.css';
 
 const GroupMainView = () => {
     const imgStyle = {
