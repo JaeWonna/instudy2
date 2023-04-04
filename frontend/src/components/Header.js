288c497fc80396068e725f691af72d9cdbe78c54
hido02 <bestdohee@gmail.com>
2023-04-04 23:07:28 +0900

    [front]STYLE: 헤더 mdb 적용(가운데 정렬 필요)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,8 +4,62 @@ import { Nav, Navbar, Button, Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 // import Header from './Header';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
-const Header = (props) => {
+// const Header = (props) => {
+
+//     const MainHeader = () => {
+//         return (
+//             <>
+//             <div>메인 페이지</div>
+//             </>
+//         );
+//     };
+
+//     const GroupHeader = () => {
+//         return (
+//             <>
+//             <div>그룹 페이지</div>
+//             </>
+//         );
+//     };
+
+//     const ProfileHeader = () => {
+//         return (
+//             <>
+//             <div>프로필 페이지</div>
+//             </>
+//         );
+//     };
+
+//     console.log(props.menu);
+//     return (
+//         <>
+//         <div class="container">
+//                        <div className="TextBox">
+//                         <div>
+//                         <Routes>
+//                        <Route path="/" element={<MainHeader/>}/>
+//             <Route path="/group" element={<GroupHeader/>}/>
+//             <Route path="/profile" element={<ProfileHeader/>}/>
+//             </Routes>
+//                         </div>
+//            </div>
+//            <div class="row">
+//            <div class="col-8">col-8</div>
+//            <div class="col-4"><FontAwesomeIcon icon={faBell} /></div>
+//            </div>
+          
+//            <hr/>
+//            </div>
+//         </>
+//     );
+// };
+
+// export default Header;
+
+const Header = () => {
 
     const MainHeader = () => {
         return (
@@ -31,11 +85,16 @@ const Header = (props) => {
         );
     };
 
-    console.log(props.menu);
+    const textStyle = {
+        marginTop: '58px',
+    }
     return (
         <>
-        <Container>
-                       <div className="TextBox">
+        <header>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+    <div class="container-fluid">
+    <div className="TextBox">
                         <div>
                         <Routes>
                        <Route path="/" element={<MainHeader/>}/>
@@ -44,8 +103,17 @@ const Header = (props) => {
             </Routes>
                         </div>
            </div>
-           <hr/>
-           </Container>
+           <div class="row">
+           <div class="col-8"></div>
+           <div class="col-4"><FontAwesomeIcon icon={faBell} /></div>
+           </div>
+    </div>
+  </nav>
+
+  <div class="p-5 text-center bg-light" style={textStyle}>
+    <h1 class="mb-3">Heading</h1>
+  </div>
+</header>
         </>
     );
 };
