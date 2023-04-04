d7f2956f9bc948df2aa6208984f03158b898d26a
wlstnam <wlstnam99@gmail.com>
2023-04-04 21:25:21 +0900

    [back]FEAT: 그룹 도메인 작성중

--- instudy/src/main/java/instudy/instudy/domain/Group.java
+++ instudy/src/main/java/instudy/instudy/domain/Group.java
@@ -1,37 +1,57 @@
 package instudy.instudy.domain;
 
-//import javax.persistence.Entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 //@Entity
 public class Group {
 
-//    @Id
-//    private String id;
-//    private String password;
-//    private String groupName;
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getGroupName() {
-//        return groupName;
-//    }
-//
-//    public void setGroupName(String groupName) {
-//        this.groupName = groupName;
-//    }
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupId;
+    private String groupName;
+    private String description;
+    private Long fixedNumber;
+    private Long currentNumber;
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getFixedNumber() {
+        return fixedNumber;
+    }
+
+    public void setFixedNumber(Long fixedNumber) {
+        this.fixedNumber = fixedNumber;
+    }
+
+    public Long getCurrentNumber() {
+        return currentNumber;
+    }
+
+    public void setCurrentNumber(Long currentNumber) {
+        this.currentNumber = currentNumber;
+    }
 }
