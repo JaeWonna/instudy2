63bc220318b050fa41d8a43a98eec974c32b7e8d
wlstnam <wlstnam99@gmail.com>
2023-04-05 09:57:06 +0900

    [back]FIX: 예약어 사용으로 인한 오류 수정

--- instudy/src/main/java/instudy/instudy/domain/StudyGroup.java
+++ instudy/src/main/java/instudy/instudy/domain/StudyGroup.java
@@ -5,8 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-//@Entity
-public class Group {
+@Entity
+public class StudyGroup {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long groupId;
